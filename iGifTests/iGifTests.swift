c59a76f35673b5013b95dbc62fb437eff9ae8d97
Whodattg <Fuad@apples-macbook.local>
2018-07-26 17:11:13 +0100

    First commit.

--- iGifTests/iGifTests.swift
+++ iGifTests/iGifTests.swift
@@ -31,37 +31,72 @@ import SwiftyJSON
 @testable import iGif
 
 class iGifTests: XCTestCase {
-  
-  let obj = ["array":["foo","bar"], "foo":"bar"] as [String : Any]
-  let request = URLRequest(url: URL(string: "http://raywenderlich.com")!)
-  let errorRequest = URLRequest(url: URL(string: "http://rw.com")!)
-  
-  override func setUp() {
-    super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    stub(condition: isHost("raywenderlich.com")) { _ in
-      return OHHTTPStubsResponse(jsonObject: self.obj, statusCode: 200, headers: nil)
+    
+    let obj = ["array":["foo","bar"], "foo":"bar"] as [String : Any]
+    let request = URLRequest(url: URL(string: "http://raywenderlich.com")!)
+    let errorRequest = URLRequest(url: URL(string: "http://rw.com")!)
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        stub(condition: isHost("raywenderlich.com")) { _ in
+            return OHHTTPStubsResponse(jsonObject: self.obj, statusCode: 200, headers: nil)
+        }
+        stub(condition: isHost("rw.com")) { _ in
+            return OHHTTPStubsResponse(error: RxURLSessionError.unknown)
+        }
     }
-    stub(condition: isHost("rw.com")) { _ in
-      return OHHTTPStubsResponse(error: RxURLSessionError.unknown)
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        OHHTTPStubs.removeAllStubs()
     }
-  }
-  
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
-    OHHTTPStubs.removeAllStubs()
-  }
-  
-  
+    
+    func testData() {
+        let observable = URLSession.shared.rx.data(request: self.request)
+        expect(observable.toBlocking().firstOrNil()).toNot(beNil())
+    }
+    
+    func testString() {
+        let observable = URLSession.shared.rx.string(request: self.request)
+        let string = "{\"array\":[\"foo\",\"bar\"],\"foo\":\"bar\"}"
+        expect(observable.toBlocking().firstOrNil()) == string
+    }
+    
+    func testJSON() {
+        let observable = URLSession.shared.rx.json(request: self.request)
+        let string = "{\"array\":[\"foo\",\"bar\"],\"foo\":\"bar\"}"
+        let json = try? JSON(data: string.data(using: .utf8)!)
+        expect(observable.toBlocking().firstOrNil()) == json
+    }
+    
+    func testError() {
+        var erroredCorrectly = false
+        let observable = URLSession.shared.rx.json(request: self.errorRequest)
+        
+        do {
+            let _ = try observable.toBlocking().first()
+            assertionFailure()
+        } catch (RxURLSessionError.unknown) {
+            erroredCorrectly = true
+        } catch {
+            assertionFailure()
+        }
+        
+        expect(erroredCorrectly) == true
+    }
+    
+    
 }
 
 extension BlockingObservable {
-  func firstOrNil() -> E? {
-    do {
-      return try first()
-    } catch {
-      return nil
+    func firstOrNil() -> E? {
+        do {
+            return try first()
+        } catch {
+            return nil
+        }
     }
-  }
 }
+
